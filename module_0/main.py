f8080395d74d940a45343ab886b3c55b594d5f7d
PolinaRuska <ruskapo10@gmail.com>
2020-04-19 01:07:16 +0300

    added code to main.py

--- module_0/main.py
+++ module_0/main.py
@@ -1,1 +1,21 @@
-print("Hello world")
+#generate random number
+import random
+num = random.randint(1, 100)
+
+#binary search to guess number & count guesses
+def computer_guess(num):
+    low = 1
+    high = 100
+    guess = 50
+    count=0
+    while guess != num:
+        guess = (low+high)//2
+        count+=1
+        if guess > num:
+            high = guess
+        elif guess < num:
+            low = guess + 1
+    print('The computer guessed {} from the {} time'.format(guess,count))
+
+#launch computer guess function
+computer_guess(num)
