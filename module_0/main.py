ece8f3b233b9a95c11fd66296ae18f658f5108d0
Andrew <rizn@rizn.org>
2020-04-26 22:15:15 +0300

    More laconic and easier to read
    √ Some variables were defined just one time
    √ Actually, there is no need to use "elif" operator at all
    √ Was chosen one style for writing assignment operators

--- module_0/main.py
+++ module_0/main.py
@@ -1,21 +1,22 @@
-#generate random number
 import random
-num = random.randint(1, 100)
+
+#generate random number from low to high
+low = 1
+high = 100
+num = random.randint(low, high)
 
 #binary search to guess number & count guesses
-def computer_guess(num):
-    low = 1
-    high = 100
-    guess = 50
-    count=0
+def computer_guess(num, low, high):
+    count = 0
+    guess = 0
     while guess != num:
+        count += 1
         guess = (low+high)//2
-        count+=1
         if guess > num:
             high = guess
-        elif guess < num:
+        if guess < num:
             low = guess + 1
     print('The computer guessed {} from the {} time'.format(guess,count))
 
 #launch computer guess function
-computer_guess(num)
+computer_guess(num, low, high)
